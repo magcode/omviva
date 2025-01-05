b8aa665faf64932740042d20546188bf643caa91
magcode <mdweb+github@posteo.de>
2025-01-05 17:44:39 +0100

    trying passive scanning

--- omviva.py
+++ omviva.py
@@ -6,9 +6,17 @@ import json
 from pathlib import Path
 from custom_logging import setupLogging
 from persistence import VivaPersistence
+from bleak import BleakScanner
+from bleak.backends.device import BLEDevice
+from bleak.assigned_numbers import AdvertisementDataType
+from bleak.backends.bluezdbus.advertisement_monitor import OrPattern
+from bleak.backends.bluezdbus.scanner import BlueZScannerArgs
+from bleak.backends.scanner import AdvertisementData
 
 logger = None
 config = None
+isReading = False
+scanner = None
 
 
 def getConfig():
@@ -19,6 +27,8 @@ def getConfig():
 
 
 async def sync():
+    global isReading
+    isReading = True
     viva = OmronBLE(logger=logger, bleAddr=config["VIVA_MAC"])
     persistence = VivaPersistence(db_name="viva_measurements.db")
 
@@ -39,9 +49,11 @@ async def sync():
     if lastSeq is None:
         lastSeq = 0
     logger.info(f"Last sequence for user #{user} was {lastSeq}")
-    
+
     try:
+        await asyncio.sleep(2)
         await viva.connect()
+        logger.info(f"Syncing user #{user}")
         allRecs = await viva.get_records(user, lastSeq + 1)
         for rec in allRecs:
             persistence.persist_measurement(rec)
@@ -51,6 +63,8 @@ async def sync():
         logger.error(f"Error: {e}")
     finally:
         persistence.close()
+    
+    isReading = False
 
 
 async def pair():
@@ -65,16 +79,40 @@ async def pair():
         logger.error(f"Error: {e}")
 
 
-async def main():
-    global config
-    global logger
+async def simple_callback(device: BLEDevice, advertisement_data: AdvertisementData):
+    if device.address == config["VIVA_MAC"] and isReading is False:
+        logger.info(f"I found {device.name}")
+        await scanner.stop()
+        await sync()
+
+
+async def scan():
+    global scanner
+    scanner = BleakScanner(
+        simple_callback,
+        None,
+        scanning_mode="passive",
+        bluez=BlueZScannerArgs(
+            or_patterns=[
+                OrPattern(0, AdvertisementDataType.FLAGS, b"\x06"),
+            ]
+        ),
+    )
+
+    while True:
+        if isReading is False:
+            #logger.info("(Re)starting scanner for Omron VIVA")
+            await scanner.start()
+            await asyncio.sleep(10.0)
+            await scanner.stop()
+        else:
+            logger.info("Waiting for sync to finish")
+            await asyncio.sleep(60.0)
+
+
+if __name__ == "__main__":
     config = getConfig()
     logger = logging.getLogger("omviva")
     setupLogging(logger, config)
-    logger.info("Started")
-
-    await sync()
-    # await pair()
-
-
-asyncio.run(main())
+    logger.info("Omron VIVA Sync Tool started")
+    asyncio.run(scan())
